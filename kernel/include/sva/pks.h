99fd28f617b90dfac5fbdca0a4abb1cc13f97044
icegrave0391 <chuqiz@comp.nus.edu.sg>
2024-05-19 14:28:32 -0700

    WP include

--- kernel/include/sva/pks.h
+++ kernel/include/sva/pks.h
@@ -3,9 +3,6 @@
 
 #include <asm/cpufeature.h>
 
-extern void pks_update_mapping(uintptr_t, int);
-extern void pks_set_key(int, bool, bool);
-
 /* Intel-defined CPU features, CPUID level 0x00000007:0 (ECX), word 16 */
 #ifndef X86_FEATURE_PKS
 #define X86_FEATURE_PKS	(16*32+31) /* Protection Keys for Supervisor pages */
@@ -21,5 +18,11 @@ static inline int check_pks_available(void)
         return 1;
     return 0;
 }
+
+extern void set_page_protection(uintptr_t virtual_page, int should_protect);
+
+/* chuqi: this is changed to `set_page_protection` */
+// extern void pks_update_mapping(uintptr_t, int);
+extern void pks_set_key(int, bool, bool);
 
 #endif // SVA_PKS_H
