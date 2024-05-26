e2947a472e291c2661067db144058d8ad9396c1f
icegrave0391 <chuqiz@comp.nus.edu.sg>
2024-05-26 00:43:06 -0700

    x

--- scripts/run-kvm-vm.sh
+++ scripts/run-kvm-vm.sh
@@ -33,11 +33,11 @@ fi
 # the 'max' version of the emulation provides all 
 # CPU features (including our needed PKS)
 sudo qemu-system-x86_64 -enable-kvm -cpu host,-pdpe1gb -smp 8,maxcpus=8\
-    $GDB\
     -m $VMMEM -no-reboot -netdev user,id=vmnic,hostfwd=tcp::8000-:22\
     -device e1000,netdev=vmnic,romfile= -drive file=$VMDISK,if=none,id=disk0,format=qcow2\
     -device virtio-scsi-pci,id=scsi0,disable-legacy=on,iommu_platform=true \
-    -device scsi-hd,drive=disk0 -nographic -monitor pty -monitor unix:monitor,server,nowait
+    -device scsi-hd,drive=disk0 -nographic -monitor pty -monitor unix:monitor,server,nowait\
+    $GDB
 
 # restore the mapping
 stty intr ^c
