2990c2ae5e0050da90d1af7d5df2cff5549e5feb
Icegrave0391 <zhangchuqi1999@gmail.com>
2024-06-13 15:04:51 -0700

    x

--- microbench/ncopy.sh
+++ microbench/ncopy.sh
@@ -1,5 +1,11 @@
 #! /bin/bash -e
 
+PORT="$1"
+
+if [[ -z $PORT ]]; then
+	PORT=8000
+fi
+
 source ../scripts/.env
-ssh -p 8000 $USERNAME@localhost "mkdir -p microbench/"
-scp -r -P 8000 ./* $USERNAME@localhost:/home/$USERNAME/microbench
+ssh -p $PORT $USERNAME@localhost "mkdir -p microbench/"
+scp -r -P $PORT ./* $USERNAME@localhost:/home/$USERNAME/microbench
