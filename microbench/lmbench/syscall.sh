43001df78df7522bfb0094585d0b42191cd1af17
icegrave0391 <chuqiz@comp.nus.edu.sg>
2024-06-25 00:52:18 -0700

    x

--- microbench/lmbench/syscall.sh
+++ microbench/lmbench/syscall.sh
@@ -6,8 +6,8 @@ source .env
 echo "Running lmbench syscal benchmark for open+close on a largefile $LARGEFILE."
 $lmbench_syscall -P $PARALLEL -W $WARMUP -N $NTIMES open $LARGEFILE
 
-echo "Running lmbench syscal benchmark for null (getpid)."
-$lmbench_syscall -P $PARALLEL -W $WARMUP -N $NTIMES null
+#echo "Running lmbench syscal benchmark for null (getpid)."
+#$lmbench_syscall -P $PARALLEL -W $WARMUP -N $NTIMES null
 
 # echo "Running lmbench syscal benchmark for stat."
 # $lmbench_syscall -P $PARALLEL -W $WARMUP -N $NTIMES stat
